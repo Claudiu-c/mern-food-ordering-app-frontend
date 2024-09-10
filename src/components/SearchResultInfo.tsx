3ae5f444f200abcf2cc4fba703fbdc7b3e8ad4d3
Claudiu-c <claudiu.capatana1@gmail.com>
2024-09-10 15:46:23 +0300

    added search page and filters

---
+++ src/components/SearchResultInfo.tsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+
+type Props = {
+  total: number;
+  city: string;
+};
+
+const SearchResultInfo = ({ city, total }: Props) => {
+  return (
+    <div className="text-xl font-bold flex flex-col gap-3 justify-between lg:items-center lg:flex-row">
+      <span>
+        {total} Restaurants found in {city}
+        <Link
+          className="ml-1 text-sm font-semibold underline cursor-pointer text-blue-500"
+          to="/"
+        >
+          Change Location
+        </Link>
+      </span>
+    </div>
+  );
+};
+
+export default SearchResultInfo;
